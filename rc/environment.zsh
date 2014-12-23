795f02e01adca78a339fa52373a574ae84a69b8c
Jean-François Roche <jfroche@affinitic.be>
2014-12-23 01:36:36 +0100

    initial import

---
+++ rc/environment.zsh
@@ -0,0 +1,8 @@
+setopt ALL_EXPORT
+HISTFILE=~/.history
+HISTSIZE=16384
+SAVEHIST=16384
+EDITOR=vim
+TZ=Europe/Brussels
+GPGKEY=69EA19A0
+GREP_OPTIONS="--color --exclude-dir=".git" --exclude=*.svn-base --exclude=*.svn --exclude=entries --exclude=all-wcprops --exclude=*.pyc -Ri"

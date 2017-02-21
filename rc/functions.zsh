b17def21429087b4d6b49c473759b6f27c5c2d35
Jean-Francois Roche <jfroche@affinitic.be>
2017-02-21 22:05:07 +0100

    drop old config

--- rc/functions.zsh
+++
@@ -1,6 +0,0 @@
-stop-mouse() {
-  xinput set-prop 11 "Device Enabled" 0
-}
-start-mouse() {
-  xinput set-prop 11 "Device Enabled" 1
-}
--- rc/options.zsh
+++
@@ -1,6 +0,0 @@
-setopt   notify globdots correct cdablevars autolist
-setopt   correctall autocd recexact longlistjobs
-setopt   autoresume histignoredups pushdsilent noclobber
-setopt   autopushd pushdminus extendedglob rcquotes mailwarning
-unsetopt bgnice autoparamslash
-setopt SHARE_HISTORY
